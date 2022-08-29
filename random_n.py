a5c927695e289df225fa3d5366e3cebaa6953070
Yeseul Han aka.Tasha <104334219+Madung2@users.noreply.github.com>
2022-08-29 23:17:50 +0900

    Add files via upload

---
+++ random_n.py
@@ -0,0 +1,22 @@
+from random import randrange
+from math import floor
+
+
+
+
+def get_1_or_0():
+    return randrange(2)
+
+
+def get_random(n):
+    '''0~n사이의 정수를 랜덤으로 반환하는 함수'''
+    if n<0:
+        raise ValueError('Cannot get random number below zero')
+
+    binary = format(floor(n), 'b')
+    while True:
+        ls=[get_1_or_0() for i in binary]
+        result= int(''.join(map(str,ls)),2)
+        if result<=n:
+            return result
+
