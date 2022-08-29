608e696269a8d477b72d5c4a4d927a13c7c82e46
Yeseul Han aka.Tasha <104334219+Madung2@users.noreply.github.com>
2022-08-29 19:10:15 +0900

    Add files via upload

--- Quantus_quiz/random_n.py
+++ Quantus_quiz/random_n.py
@@ -8,7 +8,9 @@ def get_1_or_0():
     return randrange(2)
 
 def get_random(n):
-    '''0~n사이의 정수를 반환하는 함수'''
+    '''0~n사이의 정수를 랜덤으로 반환하는 함수'''
+    if n<0:
+        raise ValueError('Cannot get random number below zero')
 
     binary = format(floor(n), 'b')
     ls=[get_1_or_0() for i in binary]
--- Quantus_quiz/test_random_n.py
+++ Quantus_quiz/test_random_n.py
@@ -5,22 +5,22 @@ class TestQuantusQuiz(unittest.TestCase):
     def test_get_1_or_0(self):
         self.assertIn(random_n.get_1_or_0(),[0,1])
 
-
     def test_get_random(self):
-        test_list=[0,1,3,10, 2.1, 10000000000000000, ]
+        test_list=[0,1,3,10, 2.1, 10000000000000000, -3]
 
         for i in test_list:
-            if i <0:
-                self.assertRaises(ValueError)
-            num=random_n.get_random(i)
-
-            self.assertTrue(
-                num>=0 
-                and num<=i 
-                and isinstance(num, int)
-                )
-            print(i,num, True)
-        
+            if i<0:
+                self.assertRaises(ValueError, random_n.get_random, i)
+                print(i,'value error')
+            else:
+                num=random_n.get_random(i)
+                
+                self.assertTrue(
+                    num>=0 
+                    and num<=i 
+                    and isinstance(num, int)
+                    )
+                print(i, num, True)
 
 
 if __name__ == '__main__':
